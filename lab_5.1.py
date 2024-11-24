c400aa9f1232f30e9e63840372149fc8de38ac63
oleg5135 <oleg363479@gmail.com>
2024-11-24 23:27:14 +0200

    Initial commit for lab_5.2

--- lab_5.1.py
+++
@@ -1,9 +0,0 @@
-import math
-
-def h(a: float, b: float) -> float:
-    return math.pow(a, 2) - math.pow(b, 2)
-
-g = float(input("g = "))
-s = float(input("s = "))
-c = (h(g + 1.0, s) + math.pow(h(g, s + 1.0), 2)) / (1.0 + math.pow(h(math.pow(g, 2), math.pow(s, 2)), 3))
-print(f"c = {c}")
